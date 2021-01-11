45eb619e96e71e47d32a29fac5b948517817fbac
Danila <dvfeduleev@gmail.com>
2021-01-11 09:38:21 +0300

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const config = require('./config/app')
+const router = require('./router/index')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(router)
+
+const port = config.appPort
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
