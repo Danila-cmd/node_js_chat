699ccec2dd9d5cc5bb54ea9d516fb902965e43ed
Danila <dvfeduleev@gmail.com>
2021-01-13 11:32:38 +0300

    logout, modal for logout, localstorage

--- src/services/authService.js
+++ src/services/authService.js
@@ -4,7 +4,7 @@ const AuthService = {
     login: (data) => {
         return API.post('/login', data)
             .then(({data}) => {
-                API.defaults.headers['Authorization'] = `Bearer ${data.token}`
+                setHeadersAndStorage(data)
                 return data
             })
             .catch(err => {
@@ -19,7 +19,7 @@ const AuthService = {
 
         return API.post('/register', data)
             .then(({data}) => {
-                API.defaults.headers['Authorization'] = `Bearer ${data.token}`
+                setHeadersAndStorage(data)
                 return data
             })
             .catch(err => {
@@ -29,8 +29,16 @@ const AuthService = {
     },
 
     logout: () => {
-
+        API.defaults.headers['Authorization'] = ''
+        localStorage.removeItem('user')
+        localStorage.removeItem('token')
     }
+}
+
+const setHeadersAndStorage = ({user, token}) => {
+    API.defaults.headers['Authorization'] = `Bearer ${token}`
+    localStorage.setItem('user', JSON.stringify(user))
+    localStorage.setItem('token', token)
 }
 
 export default AuthService
