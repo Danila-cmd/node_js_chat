58cd456348c4730d418335cc192d7ae90e4eff10
Danila <dvfeduleev@gmail.com>
2021-01-15 10:24:43 +0300

    multer, upload ProfileImage

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,35 @@
 const User = require('../models').User
 const sequelize = require('sequelize')
 
-exports.update = (req, res) => {
+exports.update = async (req, res) => {
+
+    if (req.file) {
+        req.body.avatar = req.file.filename
+    }
+
+    if (typeof req.body.avatar !== 'undefined' && req.body.avatar === 0) delete req.body.avatar
+
+    try {
+        const [rows, result] = await User.update(req.body,
+            {
+                where: {
+                    id: req.user.id
+                },
+                returning: true,
+                individualHooks: true
+            }
+        )
+
+        const user = result[0].get({raw: true})
+        // console.log(user)
+        user.avatar = result[0].avatar
+        delete user.password
+
+        return res.send(user)
+
+    } catch (e) {
+        return res.status(500).json({error: e.message})
+    }
 
     return res.send('User Controller')
 }
