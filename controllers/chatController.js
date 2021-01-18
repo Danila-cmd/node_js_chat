a39532797475065db6b3fd4f4cbaa05b61b2a878
Danila <dvfeduleev@gmail.com>
2021-01-18 12:14:12 +0300

    chat function

---
+++ controllers/chatController.js
@@ -0,0 +1,111 @@
+const models = require('../models')
+const User = models.User
+const Chat = models.Chat
+const ChatUser = models.ChatUser
+const Message = models.Message
+const {Op} = require('sequelize')
+
+exports.index = async (req, res) => {
+    const user = await User.findOne({
+        where: {
+            id: req.user.id
+        },
+        include: [
+            {
+                model: Chat,
+                include: [
+                    {
+                        model: User,
+                        where: {
+                            [Op.not]: {
+                                id: req.user.id
+                            }
+                        }
+                    },
+                    {
+                        model: Message,
+                        limit: 20,
+                        order: [['id', 'DESC']]
+                    }
+                ]
+            }
+        ]
+    })
+    return res.send(user.Chats)
+}
+
+exports.create = async (req, res) => {
+
+    const {partnerId} = req.body
+    const t = await sequelize.transaction();
+
+    try {
+
+        const user = await User.findOne({
+            where: {
+                id: req.user.id
+            },
+            include: [
+                {
+                    model: Chat,
+                    where: {
+                        type: 'dual'
+                    },
+                    include: [
+                        {
+                            model: ChatUser,
+                            where: {
+                                userId: partnerId
+                            }
+                        }
+                    ]
+                }
+            ]
+        })
+
+        if (user && user.Chats.length > 0) {
+            return res.status(403).json({status: 'Error', message: 'Chat with this user already exists!'})
+        }
+
+        const chat = await Chat.create({type: 'dual'})
+
+        await ChatUser.bulkCreate([
+            {
+                chatId: chat.id,
+                userId: req.user.id
+            },
+            {
+                chatId: chat.id,
+                userId: partnerId
+            },
+        ])
+
+        await t.commit()
+
+        const chatNew = await Chat.findOne({
+            where: {
+                id: chat.id
+            },
+            include: [
+                {
+                    model: User,
+                    where: {
+                        [Op.not]: {
+                            id: req.user.id
+                        }
+                    }
+                },
+                {
+                    model: Message
+                }
+            ]
+        })
+
+        return res.send(chat)
+
+    } catch (e) {
+        await t.rollback()
+        return res.status(500).json({status: 'Error', message: e.message})
+    }
+
+}
---
+++ router/chat.js
@@ -0,0 +1,9 @@
+const router = require('express').Router()
+const {index, create} = require('../controllers/chatController')
+const {validate} = require('../validators')
+const {auth} = require('../middleware/auth')
+
+router.get('/', [auth], index)
+router.post('/create', [auth], create)
+
+module.exports = router
